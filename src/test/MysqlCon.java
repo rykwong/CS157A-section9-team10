3abcc9b3409178e8353c52672211924404fa8929
someguy13 <ryan.kwong@gmail.com>
2020-10-26 18:23:10 -0700

    added test java file

---
+++ src/test/MysqlCon.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.sql.*;
+class MysqlCon{
+public static void main(String args[]){
+	try{
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/cs157a?serverTimezone=UTC","root","9Cn99N54!");
+		//here cs157a is database name, root is username and password
+		Statement stmt=con.createStatement();
+		ResultSet rs=stmt.executeQuery("select * from HW1");
+		while(rs.next())
+			System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3));
+		con.close();
+		}catch(Exception e){ System.out.println(e);}
+		}
+	}
